fbe2c23f2a5e56fd0d4a4ef56efbc77e3db1bb36
DanielRC24 <ramoscastro357@gmail.com>
2025-03-26 21:54:27 -0500

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
 import './bootstrap';
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Categorias from "./pages/Categorias";
+import Subcategorias from "./pages/Subcategorias";
+import Productos from "./pages/Productos";
+import ProductoDetalle from "./pages/ProductoDetalle";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Categorias} />
+        <Route exact path="/subcategorias/:categoriaId" component={Subcategorias} />
+        <Route exact path="/productos/:subcategoriaId" component={Productos} />
+        <Route exact path="/producto/:productoId" component={ProductoDetalle} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
